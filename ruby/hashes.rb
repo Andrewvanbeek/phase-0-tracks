3a637557baad045c11d94eb2df58e444d6c62e01
Andrew Van Beek <andrewvanbeek90@gmail.com>
2016-08-30 13:16:34 -0700

    finish 5.2

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -20,7 +20,32 @@
 
  Interiordesign[:additonal] = gets.chomp
 
- puts "You wrote for client: #{Interiordesign[:clientname]}, region: #{Interiordesign[:region]}, preferences: #{Interiordesign[:specificpreferences]}.  Is this correct?"
+ puts "You wrote for client: #{Interiordesign[:clientname]}, region: #{Interiordesign[:region]}, preferences: #{Interiordesign[:specificpreferences]}, additional comments: #{Interiordesign[:additonal]}.  Is this correct?"
 
+ puts "Do you need to update any of these sections? If so write Yes."
+
+update_fields = "Yes"
+
+if update_field = "Yes"
+
+	 puts "What is the Client Name?"
+ Interiordesign[:clientname] = gets.chomp
+
+ puts "What Region do they live in?"
+
+ Interiordesign[:region] = gets.chomp
+
+ puts "What are there specific preferences?"
+
+ Interiordesign[:specificpreferences] = gets.chomp
+
+ puts "Is there any additonal you want to add?"
+
+ Interiordesign[:additonal] = gets.chomp
+
+else
+
+	puts "ok awesome"
+end
  
 
