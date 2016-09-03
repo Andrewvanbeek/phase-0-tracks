ddc960d7cd7cd4e2ff91d1f1284e17f83ee245b1
Andrew Van Beek <andrewvanbeek90@gmail.com>
2016-09-02 17:22:24 -0700

    finished solo challenge and added pseudo code

--- ruby/5.5.rb
+++ ruby/5.5.rb
@@ -6,18 +6,21 @@
 #then join everything back together with join
 # p result
 
-def aliasmethod(name)
-	name = name.downcase
+def aliasmethod(name) # Define the method
+	name = name.downcase #downcase any input
 	# p name
-	name = name.split(' ')
-	# p name
-	alias_array = []
-	alias_array1 = name[1]
-	alias_array2 = name[0]
+	name = name.split(' ') #turn name into an array
+	 if name.length == 3  #create an if statement for people with a space in the last name like me.
+	 	name[1] = name[1] + name[2]  #if there are three array elements when a spilit is created then this statement will add the first part of the lastname to the second part of the lastname.
+	 	name.delete_at(2) #this part of the code will delete third uncessary array element
+	 else
+	 end
+	alias_array1 = name[1]  #takes the lastname and stores it into a new array(but could be var as well I think).
+	alias_array2 = name[0]  #takes the firstname and stores it into a new array(could also be a variable I just went with an array index).
 	# p alias_array1
-	alias_array1 = alias_array1.split('')
+	alias_array1 = alias_array1.split('')  #spilits up the first array into different letters, and those letters become different elements of the array.
 	# p alias_array1
-	newaliasarray1 = alias_array1.map do |x|
+	newaliasarray1 = alias_array1.map do |x|  #iterates an if and else statement on all the letters of the lastname in the array.  One portion is for the vowels.  It maps the elements of array1 into a new array of newaliasarray1.
 		arrayvowels = ["a", "e", "i", "o", "u"]
 	if x == arrayvowels[0]
 		x = "e"
@@ -29,18 +32,18 @@ def aliasmethod(name)
 		x = "u"
 	elsif x == arrayvowels[4]
 		x = "a"
-	elsif x.next == "aa"
+	elsif x.next == "aa" #this is built for z specifically whoes next letter is aa.
 		x = "b"
-	elsif x.next == arrayvowels[1] 
+	elsif x.next == arrayvowels[1] #this portion is for letters whose next letter is vowel.  It says it will actually skip over the vowel if a vowel is the next letter if the original letter is a consant.
 		x = x.next.next
 	elsif x.next == arrayvowels[2] 
 		x = x.next.next
 	elsif x.next == arrayvowels[3] 
 		x = x.next.next
-	elsif x.next == arrayvowels[4]
+	elsif x.next == arrayvowels[4] # this is where the consant rule with vowels end.
 		x = x.next.next
 	else 
-	x = x.next 
+	x = x.next #this is the rule with rest of the consants that do not have vowels as the next letter.  it just says x
 	end
 end
 
@@ -48,7 +51,7 @@ end
 	alias_array2 = alias_array2.split('')
 	# p alias_array2
 
-newaliasarray2 = alias_array2.map do |x|
+newaliasarray2 = alias_array2.map do |x|  #this map function does the same thing for the firstname that the prior map statement did for the last name.
 		arrayvowels = ["a", "e", "i", "o", "u"]
 	if x == arrayvowels[0]
 		x = "e"
@@ -79,18 +82,28 @@ end
 # p newaliasarray1
 # p newaliasarray2
 	# p newaliasarray
-	newaliasarray1 = newaliasarray1.join('')
-	newaliasarray1 = newaliasarray1 + " "
-	newaliasarray1 = newaliasarray1.capitalize
+	newaliasarray1 = newaliasarray1.join('')  #joins the lastname letter array elements into a string again
+	newaliasarray1 = newaliasarray1 + " " #adds a space to the string
+	newaliasarray1 = newaliasarray1.capitalize #capitalizes the first letter of the lastname
 	# p newaliasarray1
-	newaliasarray2 = newaliasarray2.join('')
-	newaliasarray2 = newaliasarray2.capitalize
-	finalnewaliasarray = []
-	finalnewaliasarray[0] = newaliasarray1
-	finalnewaliasarray[1] = newaliasarray2
-	finalnewaliasarray = finalnewaliasarray.join('')
-	return finalnewaliasarray
+	newaliasarray2 = newaliasarray2.join('') #turns the original firstname into a string.
+	newaliasarray2 = newaliasarray2.capitalize #capitalizes the first letter
+	finalnewaliasarray = [] #creates new array where the first name and last name can join
+	finalnewaliasarray[0] = newaliasarray1 #adding the last name in the first index of the array
+	finalnewaliasarray[1] = newaliasarray2 #adding the firstname in second part of the array
+	finalnewaliasarray = finalnewaliasarray.join('') #joining the array
+	return finalnewaliasarray #returning the resultin
 	# p newaliasarray
 end
 
-p aliasmethod("Felicia Torres")
+
+input_var = 0 #will be our var in our method
+
+until input_var == "quit" do #until loop to run into user writes quit
+
+puts "Hey put in your name! First name and Last name that is! Write quit if you are done. If you have lastname with a space that is fine but please no names more than 3 words or else you will get this message again. For example Andrew Van Beek is ok but Andrew Ryan Van Beek is not ok because it is more then 4 words.  also just write quit when you are done."
+
+input_var = gets.chomp
+
+	p aliasmethod(input_var)
+end
