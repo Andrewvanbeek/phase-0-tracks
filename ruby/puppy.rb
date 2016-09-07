c5dd1dfc7827c76b8b90d388bcb2af53af852334
Andrew Van Beek <andrewvanbeek90@gmail.com>
2016-09-07 13:38:32 -0700

    adds methods

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -15,3 +15,28 @@
 #Wagging their tail
 #Having fun!
 #Barking!
+
+
+
+
+class Puppy
+	def speak(n)
+		n.times do |n|
+			puts "woof!"
+		end
+	end
+
+	def rollover
+		puts "rollover"
+	end
+
+	def dogyears(n)
+		dogy = ((n - 2) * 4) + (10.5 * 2) #formula of dog years is that the firt two years of a dog life 10.5 years, every other year after that is 4
+		doy 
+
+
+end
+
+duchess = Puppy.new
+
+duchess.speak(5)
