3a5dcf7a01ea1db614c60b5e2d4ce021e4ff3cd6
Andrew Van Beek <andrewvanbeek90@gmail.com>
2016-09-11 18:12:44 -0700

    pushes files to github

---
+++ ruby/agents2.rb
@@ -0,0 +1,52 @@
+#encryption method
+#method that takes any string and advances it one letter using.next.  Do this by create a new string.
+# #string is turned into an array and rejoined
+
+
+def encryption(string)
+	new_string_array = string.split("") #abc = [a, b, c]
+	x = 0
+	while x < new_string_array.length
+	new_string_array[x] = new_string_array[x].next
+	x += 1
+	end
+	puts new_string_array.join("")
+end
+
+# def encryption(string)
+# 	new_string = ""
+# 	x = 0
+# 	while x < string.length
+		
+# 		if string[x] == "z"
+# 			new_string = new_string + "a"
+# 		else
+# 			new_string = new_string + string[x].next
+# 	end
+			
+# 	x += 1
+# end
+# 	puts new_string
+# 	new_string
+# end
+
+
+# def decryption(new_string)
+# 	alphabet = "abcdefghijklmnopqrstuvwxyz"
+# 	y = 0
+# 	newstringd = ""
+# 	while y < new_string.length
+# 	tochange = alphabet[alphabet.index(new_string[y]) - 1]
+# 	newstringd = newstringd + tochange
+# 	puts newstringd
+# 	y += 1
+# 	end 
+	
+# end 
+
+
+
+
+#  output = encryption("abc")
+
+ # p decryption(output)
--- ruby/gamedesign/gdesignfinal.rb
+++ ruby/gamedesign/gdesignfinal.rb
@@ -10,6 +10,7 @@
 #if there is a guess
 #should be another method that asks to guess the letters of the firstphrase and printout if the letters are in the word of the phrase.
 
+
 class Game
 
 	# attr_reader :something
@@ -70,7 +71,6 @@ class Game
 		end
 		@arrayofusedletters << input
 		@x += 1
-		@arrayofusedletters
 	end
 
 	def wordguess
@@ -148,113 +148,3 @@ end
 
 
 puts "Thanks for playing."
-
-
-# class Game
-
-# 	attr_reader :something
-# 	attr_accessor :firstphrase
-# 	attr_accessor :secondphrase
-# 	attr_accessor :arr
-# 	attr_accessor :array2
-
-# 	def initialize(firstphrase)
-# 		@firstphrase = firstphrase
-# 		@arr = @firstphrase.split('')
-# 		@array2 = (1..@firstphrase.length).to_a
-# 		@array2.map! do |a|
-# 			"_"
-# 		end
-# 	end
-
-# 	def guess(secondphrase)
-# 		@secondphrase = secondphrase
-# 		if @secondphrase == @firstphrase
-# 			puts "you guessed it!"
-# 		else
-# 			puts "Sorry nope! Try again"
-# 		end
-# 		@secondphrase == @firstphrase
-# 	end
-
-# 	def singleletterguess(input)
-
-# 		@arr.each do |b|
-# 			if b == input
-# 				@array2[@arr.index(b)] = b
-# 				@arr[@arr.index(b)] = 0
-
-# 			end
-# 		end
-
-# 		puts "#{@array2.join}"
-# 		@array2
-# 	end
-
-# end
-
-
-
-
-
-
-
-
-# puts "what is the word to be guessed?"
-
-# require 'io/console'
-
-# pw = STDIN.noecho(&:gets).chomp.upcase #hides the text of what is being typed
-
-# phrase1phase1 = Game.new(pw)
-
-# puts phrase1phase1.firstphrase + "!"
-
-# x = 0
-
-# arrayofusedletters = []
-
-
-# while x < phrase1phase1.firstphrase.length
-
-# 	puts "Can you guess the letters(If you guess the same letter twice it won't be counted against you)? #{phrase1phase1.firstphrase.length - x} trys available."
-
-# 	lettergues = gets.chomp.upcase
-
-# 	phrase1phase1.singleletterguess(lettergues)
-
-# 	# p @randomletterstring
-
-
-# 	if arrayofusedletters.include?(lettergues)
-# 		x = x - 1
-# 	end
-
-# 	arrayofusedletters << lettergues
-
-# 	x += 1
-
-# 	puts "Are you ready for you for your word guess? If so write yes(Only write yes if you are sure you don't need letters). If you write anything else you will guess again."
-
-
-
-# 	finalguess = gets.chomp.upcase
-
-# 	if finalguess == "YES"
-
-# 	puts "what is the word?"
-
-# 	phrase2whattodo = gets.chomp.upcase
-
-# 	phrase1phase1.guess(phrase2whattodo)
-
-# 	x += 1
-# else 
-# 	puts "ok try guessing the letter again"
-# end
-# 	if phrase1phase1.firstphrase == phrase1phase1.secondphrase
-# 		x = phrase1phase1.firstphrase.length
-# 	end
-# end
-
-# puts "Thanks for playing."
