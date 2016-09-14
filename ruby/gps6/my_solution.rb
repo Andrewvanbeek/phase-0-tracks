aa53c81514dace027db1c44fcdcf43089d632f9d
Andrew Van Beek <andrewvanbeek90@gmail.com>
2016-09-14 09:42:05 -0700

    creates a hash to easily call on the different instances created in the .each iteration.  also fixes bug where states were not printing out the proper statement

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -10,13 +10,16 @@ require_relative 'state_data'
 
 class VirusPredictor
 
+attr_accessor :state, :population, :population_density
 
 # initialize method is responsible for creating instances and taking the vatiable given 
 
   def initialize(state_of_origin, population_density, population)
+    puts "creating"
     @state = state_of_origin
     @population = population
     @population_density = population_density
+
   end
 
 
@@ -33,7 +36,7 @@ private
 
 # Taking the info from the other file, comparing to the if statement, and doing to correspsonding math. Then prints a statement with the state and the answer to the equation.
   def predicted_deaths #(population_density, population, state)
-    # # predicted deaths is solely based on population density
+    # predicted deaths is solely based on population density
     # if @population_density >= 200
     #   number_of_deaths = (@population * 0.4).floor
     # elsif @population_density >= 150
@@ -47,10 +50,10 @@ private
     # end
 
     # print "#{@state} will lose #{number_of_deaths} people in this outbreak"
-    pdeathshash1 = {0..49 => (@population * 0.05).floor, 50..99 => (@population * 0.1).floor, 100..149 => (@population * 0.2).floor, 150..199 => (@population * 0.3).floor, 200..900 => (@population * 0.04).floor}
-      pdeathshash1.each do |range, death|
-      if (range).include?(@population_density)
-         print "#{@state} will lose #{death} people in this outbreak"
+    pdeathshash1 = {0..49 => (@population * 0.05).floor, 50..99 => (@population * 0.1).floor, 100..149 => (@population * 0.2).floor, 150..199 => (@population * 0.3).floor, 200..10000000 => (@population * 0.04).floor}
+      pdeathshash1.each do |range, deaths|
+      if (range).include?(@population_density.floor)
+         print "#{@state} will lose #{deaths} people in this outbreak"
         end
       end
 
@@ -61,7 +64,7 @@ private
   def speed_of_spread #(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
+  
 
 #Method should be able to have add greater speed to to pop density numbers that are lower then each other at increments of 50.
 #steps 
@@ -83,12 +86,12 @@ private
 #   end
 
 # end
-    speedhash1 = {0..49 => 2.5, 50..99 => 2.0, 100..149 => 1.5, 150..199 => 1.0, 200..900 => 0.5}
+    speedhash1 = {0..49 => 2.5, 50..99 => 2.0, 100..149 => 1.5, 150..199 => 1.0, 200..103589999 => 0.5}
     speedhash1.each do |range, speed|
-        if (range).include?(@population_density)
+        if (range).include?(@population_density.floor)
          puts " and will spread across the state in #{speed} months.\n\n"
         end
-      end
+    end
 
 
     # puts " and will spread across the state in #{speed} months.\n\n"
@@ -101,17 +104,22 @@ end
 
 # DRIVER CODE
  # initialize VirusPredictor for each state
+ state_instance_hash = {}
 
-# STATE_DATA.each do |state, hashes|
-#   call = VirusPredictor.new(state, STATE_DATA[state][:population_density], STATE_DATA[state][:population])
-#   call.speed_of_spread
-# end
+STATE_DATA.each do |state, hashes|
+  state_instance_hash[state] = VirusPredictor.new( state, hashes[:population_density], hashes[:population])
+  state_instance_hash[state].virus_effects
+end
+
+# ObjectSpace.each_object(VirusPredictor) {|x| p x}
+
+p state_instance_hash["California"].state
 
 
 
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
+# alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+# alabama.virus_effects
 
 #jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
 #jersey.virus_effects
