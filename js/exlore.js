cbd140d57cbf6d604e5f53bc2563dc91320ae51d
Andrew Van Beek <andrewvanbeek90@gmail.com>
2016-09-19 09:05:07 -0700

    explore.js

---
+++ js/exlore.js
@@ -0,0 +1,18 @@
+// takes the index of var name and passes it through a loop until 
+
+
+
+
+
+var name = "hello"
+
+
+reversethename = function (name1)
+{var array = []; array.push(name1);}
+
+
+for (var i = 1; i < name.length + 1; i++) {
+	var array = []; array.push((name[name.length - i]));
+}
+
+console.log(array);
