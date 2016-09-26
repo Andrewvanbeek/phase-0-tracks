d3321b74ff4539180245606a08c78e1f37731c60
Andrew Van Beek <andrewvanbeek90@gmail.com>
2016-09-26 00:26:51 -0700

    finishes project to ask places to visit and what to do there

--- databases/8_5app/coolplaces.rb
+++ databases/8_5app/coolplaces.rb
@@ -1,13 +1,19 @@
+
+
+# Program that will ask you what is your favorite city to visit, and also what is a good thing to do there.
+#  It will then ask if there is a city you are curious about, and then it will show all the prievous suggestions of what to do in the city
+#  For example if you are curious about Hong Kong, you will write hong kong and all the activities that have to do with hong kong written by prior users will come back
+#  In this example for hong kong eating dim sum and shopping should come back
+#  Finally in a loop so it will ask you to continute until you are done.
+
+
+
+
 require 'faker'
 require 'sqlite3'
 
 database = SQLite3::Database.new ("coolplaces.db") 
 
-# do |eightfivedb|
-# 	eightfivedb.execute( "select * from table" ) do |row|
-# 		p row
-# 	end
-# end	
 
 create_table_cmd = <<-SQL 
 	CREATE TABLE IF NOT EXISTS thing_to_do (
@@ -32,33 +38,43 @@ SQL
 database.execute(create_table_cmd)
 database.execute(create_table_cmd1)
 
-puts "What is your favorite city to visit?"
+sequence_ender = ""
+until sequence_ender == "done"
 
-citytov = gets.chomp
+	puts "What is your favorite city to visit?"
 
-puts "What is your favorite thing to do there?"
+	citytov = gets.chomp.downcase
 
-do_thing = gets.chomp
+	puts "What is a good thing to do there."
 
-database.execute("INSERT INTO thing_to_do (things_to_do) VALUES (?)", [do_thing]) 
+	do_thing = gets.chomp.downcase
 
-# variable = database.execute("SELECT TOP 1 id FROM thing_to_do ORDER BY column_name DESC")
+	database.execute("INSERT INTO thing_to_do (things_to_do) VALUES (?)", [do_thing]) 
 
-# database.execute("INSERT INTO places_to_visit (city_to_visit, things_id) VALUES (#{citytov}, #{variable})")
-
-
-
-# def createnames
-# coolthings = ["batman", "kungfu", "nun chucks", "Jackie Chan", "robots"].sample
-# coolthings
-# end
-
-# def create_awesomethings(database, name, age)
-# 	database.execute("INSERT INTO awesomething (name, age) VALUES (?, ?)", [name, age])
-# end
+	idcount = database.execute("SELECT id 
+	    FROM    thing_to_do
+	    WHERE   ID = (SELECT MAX(id)  FROM thing_to_do)
+	")
+	database.execute("INSERT INTO places_to_visit (city_to_visit, things_id) VALUES (?, ?)", [citytov, idcount])
 
 
+	puts "Is there any other city you are curious about?  If so write the city name to see what activities are associated with it."
 
-# 200.times do
-# 	create_awesomethings(database, Faker::Name.name, 0)
-# end
+		othercity = gets.chomp.downcase
+
+	othercityfact = database.execute("SELECT things_to_do FROM places_to_visit
+	JOIN thing_to_do ON places_to_visit.things_id = thing_to_do.id
+	WHERE places_to_visit.city_to_visit = ? ", [othercity])
+
+	puts "Here are the things people have listed to do in that city."
+	puts othercityfact
+
+	puts "If you are done, simply write done.  If you want to add another city and activity press any key to continue."
+
+		sequence_ender = gets.chomp.downcase
+
+end
+
+
+
+
