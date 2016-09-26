b75c9c90b934a7f605067a71a4e26471eac97899
Andrew Van Beek <andrewvanbeek90@gmail.com>
2016-09-26 00:51:57 -0700

    finish the rb file

--- databases/8_5app/coolplaces.rb
+++ databases/8_5app/coolplaces.rb
@@ -2,9 +2,28 @@
 
 # Program that will ask you what is your favorite city to visit, and also what is a good thing to do there.
 #  It will then ask if there is a city you are curious about, and then it will show all the prievous suggestions of what to do in the city
-#  For example if you are curious about Hong Kong, you will write hong kong and all the activities that have to do with hong kong written by prior users will come back
+#  For example if you are curious about Hong Kong, you will write hong kong and all the activities/travel suggestions that have to do with hong kong written by prior users will come back
 #  In this example for hong kong eating dim sum and shopping should come back
 #  Finally in a loop so it will ask you to continute until you are done.
+
+#pseudocode
+
+#create a program that allows the user to say their favorite city to visit.  Then allow the user to suggest a thing to do in the city.
+#At the end users can query the database for a certain city and it will return prior suggestions of things to do in the city.
+
+#SQL side
+#TWO tables: places_to_visit and thing_to_do
+#places_to_visit will have have two main columns other then the foreign key and primary key.  Columns will be city to visit and the things_id(will work as foreign key).
+#other table of thing_to_do will just have things_to_do in the city with the primary key.
+
+#program
+#prompts user what city is their favorite.  They response gets stored in a variable.
+#Second prompt asks what they like to do in that city, and that gets store in a variable.
+
+#those variables should then be stored in the database within the tables.  Making  sure the primary id of thing_to_do matches the integer number for thing_id for a join to work properly.
+
+#the prompt will then ask the user if there is another city they are interested in.  If the sql where statement is true then and the city has been mentioned before, then all the activities suggested with that city will be displayed.
+#possible way I could improve upon this is refactoring and saving all the logic as methods rather than variables for readability.
 
 
 
@@ -39,6 +58,7 @@ database.execute(create_table_cmd)
 database.execute(create_table_cmd1)
 
 sequence_ender = ""
+
 until sequence_ender == "done"
 
 	puts "What is your favorite city to visit?"
